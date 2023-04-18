227c4e454e39716ded9694b88e8eba729019c85d
spencerhall01 <111201982+spencerhall01@users.noreply.github.com>
2023-04-17 23:57:46 -0500

    Test file to test testing

---
+++ code/test_test.py
@@ -0,0 +1,25 @@
+# test_example.py
+
+def test_addition():
+    assert 1 + 1 == 2
+
+def test_subtraction():
+    assert 3 - 1 == 2
+
+def test_multiplication():
+    assert 2 * 3 == 6
+
+def test_division():
+    assert 6 / 2 == 3
+    
+def test_login():
+
+def test_logout():
+
+def test_search():
+
+def test_addToCart():
+
+def test_removeFromCart():
+
+def 
